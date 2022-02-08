b3714e559b0a3858d7bc449debe771bed87e9b1d
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2022-02-07 21:57:53 -0800

    create a nextjs example that points to local build

--- examples/react-nextjs/pages/index.js
+++ examples/react-nextjs/pages/index.js
@@ -2,6 +2,22 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import {
+  MediaController,
+  MediaControlBar,
+  MediaPlayButton,
+  MediaSeekBackwardButton,
+  MediaSeekForwardButton,
+  MediaMuteButton,
+  MediaVolumeRange,
+  MediaTimeRange,
+  MediaTimeDisplay,
+  MediaCaptionsButton,
+  MediaPlaybackRateButton,
+  MediaPipButton,
+  MediaFullscreenButton,
+} from "media-chrome/dist/react";
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -16,40 +32,43 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+    <MediaController>
+      <video
+        slot="media"
+        muted
+        src="https://stream.mux.com/DS00Spx1CV902MCtPj5WknGlR102V5HFkDe/high.mp4"
+        crossOrigin=""
+      >
+        <track
+          label="thumbnails"
+          default
+          kind="metadata"
+          crossOrigin=""
+          src="https://image.mux.com/DS00Spx1CV902MCtPj5WknGlR102V5HFkDe/storyboard.vtt"
+        ></track>
+        <track
+          label="English"
+          kind="captions"
+          srcLang="en"
+          crossOrigin=""
+          src="./vtt/en-cc.vtt"
+        ></track>
+      </video>
+      <MediaControlBar>
+        <MediaPlayButton />
+        <MediaSeekBackwardButton />
+        <MediaSeekForwardButton />
+        <MediaMuteButton />
+        <MediaVolumeRange />
+        <MediaTimeRange />
+        <MediaTimeDisplay remaining/>
+        <MediaCaptionsButton />
+        <MediaPlaybackRateButton />
+        <MediaPipButton />
+        <MediaFullscreenButton />
+      </MediaControlBar>
+    </MediaController>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
